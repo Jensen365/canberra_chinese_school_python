7e983d69654e7c10db3e023788f7ee374b760826
Jensen365 <jensenlu365@outlook.com>
2026-02-13 23:32:03 +0000

    complete sem1_w1

--- sem1_w1.py
+++ sem1_w1.py
@@ -1,4 +1,4 @@
-print("welcome to use the grade calculater!")
+print("Welcome to use the grade calculater!")
 
 coursework_score = float(input("Enter your coursework score: "))
 final_exam_score = float(input("Enter your exam score: "))
