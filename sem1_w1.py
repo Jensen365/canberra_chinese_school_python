038e8d88d1717a9d93a6fc83d1e4b458d3922822
Jensen365 <jensenlu365@outlook.com>
2026-02-13 23:24:54 +0000

    Fixed formula

--- sem1_w1.py
+++ sem1_w1.py
@@ -1,2 +1,8 @@
-print("")
- 
+print("welcome to use the grade calculater!")
+
+coursework_score = float(input("Enter your coursework score: "))
+final_exam_score = float(input("Enter your exam score: "))
+
+final_grade = 0.4 * coursework_score + 0.6 * final_exam_score
+
+print("Your final grade is ", final_grade) 
