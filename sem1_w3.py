ecb732dfc7cbdf51a9eadef246a1ffe21a92c9ac
Jensen365 <jensenlu365@outlook.com>
2026-02-21 02:04:41 +0000

    complete

--- sem1_w3.py
+++ sem1_w3.py
@@ -1,1 +1,9 @@
-
+import random
+heath_points=[60,80,100]
+heroes=["knight","wizard","archor"]
+hero=random.choice(heroes)
+point=random.choice(heath_points)
+damage=random.randiant(10,30)
+hp_Left=point-damage
+print(hero)
+print(point)
