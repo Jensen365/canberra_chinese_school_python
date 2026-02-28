b4617104712b3e91347c5c7ff610f827d99803ea
Jensen365 <jensenlu365@outlook.com>
2026-02-28 02:05:32 +0000

    Finish Finish

--- sem1_w4.py
+++ sem1_w4.py
@@ -3,13 +3,13 @@ heath_points=[60,80,100]
 heroes=["knight","wizard","archor"]
 hero=random.choice(heroes)
 point=random.choice(heath_points)
-damage=random.randiant(10,30)
+damage=random.randint(10,30)
 hp_Left=point-damage
 print(hero)
 print(point)
 Enemy="Enemy1"
 Enemy_hp=100
-damage_to_enemy=random.randiant(15,40)
+damage_to_enemy=random.randint(15,40)
 Enemy_hp_left=Enemy_hp-damage_to_enemy
 if hp_Left>=Enemy_hp_left:
     print("hero wins")
